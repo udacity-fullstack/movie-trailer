2c4b8354b7ddb9d8e4bc808681be7b63ec2ea520
Wang Qun Feng <qunfengw@gmail.com>
2015-12-02 15:09:13 +0800

    use Jinja2 to refactor the code

--- media.py
+++ media.py
@@ -7,7 +7,7 @@ class Movie():
                   trailer_youtube_url: the youtube link of the movie trailer
                   imdb_link: the link in IMDB database
     """
-	def __init__(self, movie_title, poster_image, trailer_youtube, imdb_link):
+    def __init__(self, movie_title, poster_image, trailer_youtube, imdb_link):
 		self.title = movie_title
 		self.poster_image_url = poster_image
 		self.trailer_youtube_url = trailer_youtube
